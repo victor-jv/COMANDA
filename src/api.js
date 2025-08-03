448c0c268a4aac9fe3b397f47a8cb583e009c8fd
João Victor <112131417+victor-jv@users.noreply.github.com>
2025-08-03 09:19:08 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // ✅ agora aponta para seu backend local
+  baseURL: 'https://backendcmd.onrender.com', // ✅ agora aponta para seu backend local
 });
 
 export default api;
